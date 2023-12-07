1492270c6e3d7a09b11043129e2ef9ed8c1c5c57
Sam202003 <samruddhichavan05@gmail.com>
2023-12-07 10:27:57 +0530

    added the cover Image remove replace functionality

--- components/modals/cover-image-modal.tsx
+++ components/modals/cover-image-modal.tsx
@@ -1,8 +1,6 @@
-'use client';
+"use client";
 
-import {
-Dialog, DialogContent,DialogHeader
-} from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogHeader } from "@/components/ui/dialog";
 import { useCoverImage } from "@/hooks/use-cover-image";
 import { SingleImageDropzone } from "@/components/single-image-dropzone";
 import { useState } from "react";
@@ -11,55 +9,57 @@ import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { useParams } from "next/navigation";
 import { Id } from "@/convex/_generated/dataModel";
-export const CoverImageModal=()=>{
-    const params = useParams();
-    const update = useMutation(api.documents.update)
-    const coverImage = useCoverImage();
-    const [file,setFile] = useState<File>();
-    const [isSubmitting,setIsSubmitting] = useState(false);
-    const {edgestore} = useEdgeStore();
+export const CoverImageModal = () => {
+  const params = useParams();
+  const update = useMutation(api.documents.update);
+  const coverImage = useCoverImage();
+  const [file, setFile] = useState<File>();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const { edgestore } = useEdgeStore();
 
+  const onClose = () => {
+    setFile(undefined);
+    setIsSubmitting(false);
+    coverImage.onClose();
+  };
+  const onChange = async (file?: File) => {
+    if (file) {
+      setIsSubmitting(true);
+      setFile(file);
+      let res;
+      if (coverImage.url) {
+        res = await edgestore.publicFiles.upload({
+          file,
+          options: {
+            replaceTargetUrl: coverImage.url,
+          },
+        });
+      } else {
+        res = await edgestore.publicFiles.upload({
+          file,
+        });
+      }
 
-    const onClose = ()=>{
-        setFile(undefined);
-        setIsSubmitting(false);
-        coverImage.onClose();
+      await update({
+        id: params.documentId as Id<"documents">,
+        coverImage: res.url,
+      });
+      onClose();
     }
-    const onChange = async(file?:File)=>{
-        if(file){
-            setIsSubmitting(true);
-            setFile(file);
-
-            const res = await edgestore.publicFiles.upload({
-                file
-            });
-
-            await update ({
-              id:params.documentId as Id<"documents">,
-              coverImage: res.url
-            });
-            onClose();
-        }
-    }
-    return(
-        <Dialog open={coverImage.isOpen} onOpenChange={coverImage.onClose}>
-            <DialogContent>
-                <DialogHeader>
-                    <h2 className="text-center text-lg font-semibold">
-                        Cover Image
-                    </h2>
-                </DialogHeader>
-                <SingleImageDropzone className="w-full outline-none"
-                disabled={isSubmitting}
-                onChange={onChange}
-                />
-                <div>
-                    Todo : Upload image
-                </div>
-            </DialogContent>
-
-        </Dialog>
-    )
+  };
+  return (
+    <Dialog open={coverImage.isOpen} onOpenChange={coverImage.onClose}>
+      <DialogContent>
+        <DialogHeader>
+          <h2 className="text-center text-lg font-semibold">Cover Image</h2>
+        </DialogHeader>
+        <SingleImageDropzone
+          className="w-full outline-none"
+          disabled={isSubmitting}
+          onChange={onChange}
+        />
+        <div>Todo : Upload image</div>
+      </DialogContent>
+    </Dialog>
+  );
 };
-
-
